f7ba65fde9877796e963686b72f93ecaf520d053
prav nak <pnakshatrala@gmail.com>
2021-12-29 09:19:29 -0800

    fixed bugs.

--- 1_Intro_Threads/1_print_threadIDs.cu
+++ 1_Intro_Threads/1_print_threadIDs.cu
@@ -3,6 +3,17 @@
 
 #include <stdio.h>
 
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
 // Function to print the ID of the threads
 __global__ void print_threadIds()
 {
@@ -21,6 +32,7 @@ int main()
 	dim3 block_size(nx, ny, nz);
 
 	print_threadIds<<<grid_size, block_size>>>();
+	gpuErrchk( cudaPeekAtLastError() );
 
 	cudaDeviceSynchronize();
 	cudaDeviceReset();
